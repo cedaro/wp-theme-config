723e347966e923d2becd6ae085b6c7e271c805b5
Brady Vercher <brady@blazersix.com>
2015-02-23 12:00:21 -0800

    Add default task aliases.

---
+++ grunt/aliases.js
@@ -0,0 +1,51 @@
+module.exports = function( grunt, options ) {
+	var tasks = {
+		'build:css': [
+			'build:css:theme'
+		],
+		'build:css:theme': [
+			'less:theme',
+			'pixrem:theme',
+			'autoprefixer:theme',
+			'wpcss:theme',
+			'cssjanus:theme'
+		],
+		'build:css:plugins': [],
+		'package': [
+			'replace',
+			'build',
+			'clean:package',
+			'copy:package',
+			'addtextdomain:package',
+			'makepot:build',
+			'compress:package',
+			'clean:package'
+		]
+	};
+
+	if ( grunt.file.exists( options.paths.less + '/plugins/easy-digital-downloads.less' ) ) {
+		tasks['build:css:plugins:easy-digital-downloads'] = [
+			'less:easy-digital-downloads',
+			'pixrem:easy-digital-downloads',
+			'autoprefixer:easy-digital-downloads',
+			'wpcss:easy-digital-downloads',
+			'cssjanus:easy-digital-downloads'
+		];
+
+		tasks['build:css:plugins'].push( 'build:css:plugins:easy-digital-downloads' );
+	}
+
+	if ( grunt.file.exists( options.paths.less + '/plugins/woocommerce.less' ) ) {
+		tasks['build:css:plugins:woocommerce'] = [
+			'less:woocommerce',
+			'pixrem:woocommerce',
+			'autoprefixer:woocommerce',
+			'wpcss:woocommerce',
+			'cssjanus:woocommerce'
+		];
+
+		tasks['build:css:plugins'].push( 'build:css:plugins:woocommerce' );
+	}
+
+	return tasks;
+};
