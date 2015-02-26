e43cd1853f21496b06b962669d94ed4a60660f71
Brady Vercher <brady@blazersix.com>
2015-02-26 12:31:09 -0800

    Add WPCOM CSS tasks.

---
+++ config/grunt/wpcom-tasks.js
@@ -0,0 +1,67 @@
+module.exports = function( grunt ) {
+    var theme = {
+        autoprefixer: {
+            src: [
+                '<%= paths.wpcom %>/style.css'
+            ]
+        },
+        cssjanus: {
+            files: [
+                {
+                    src: '<%= paths.wpcom %>/style.css',
+                    dest: '<%= paths.wpcom %>/style-rtl.css',
+                }
+            ]
+        },
+        less: {
+            files: [
+                {
+                    src: '<%= paths.less %>/wpcom.less',
+                    dest: '<%= paths.wpcom %>/style.css'
+                }
+            ]
+        },
+        pixrem: {
+            files: [
+                {
+                    src: '<%= paths.wpcom %>/style.css',
+                    dest: '<%= paths.wpcom %>/style.css'
+                }
+            ]
+        },
+        wpcss: {
+            files: [
+                {
+                    src: '<%= paths.wpcom %>/style.css',
+                    dest: '<%= paths.wpcom %>/style.css'
+                }
+            ]
+        }
+    };
+
+    if ( grunt.file.exists( options.paths.less + '/editor-style.less' ) ) {
+        theme.autoprefixer.src.push( '<%= paths.wpcom %>/assets/css/editor-style.css' );
+
+        theme.cssjanus.files.push({
+            src: '<%= paths.wpcom/assets/css/editor-style.css',
+            dest: '<%= paths.wpcom/assets/css/editor-style-rtl.css'
+        });
+
+        theme.less.files.push({
+            src: '<%= paths.less %>/editor-style.less',
+            dest: '<%= paths.wpcom/assets/css/editor-style.css'
+        });
+
+        theme.pixrem.files.push({
+            src: '<%= paths.wpcom/assets/css/editor-style.css',
+            dest: '<%= paths.wpcom/assets/css/editor-style.css'
+        });
+
+        theme.wpcss.files.push({
+            src: '<%= paths.wpcom/assets/css/editor-style.css',
+            dest: '<%= paths.wpcom/assets/css/editor-style.css'
+        });
+    }
+
+    return theme;
+};
