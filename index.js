26dc9bebc363b13f41a996275ca34d45e00681e3
Brady Vercher <brady@blazersix.com>
2015-02-26 10:44:08 -0800

    Update config paths.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 module.exports = function() {
 	return {
 		paths: {
-			assets: 'assets',
-			config: 'grunt/config',
-			css:    'src/assets/css',
-			dist:   'dist',
-			js:     'src/assets/js',
-			less:   'assets/less',
-			logs:   'logs',
-			source: 'src',
-			tasks:  'grunt/tasks',
-			tmp:    'tmp',
-			wpcom:  'wpcom',
+			assets:  'assets',
+			config:  'config',
+			css:     'src/assets/css',
+			dist:    'dist',
+			grunt:   'config/grunt',
+			js:      'src/assets/js',
+			less:    'assets/less',
+			logs:    'logs',
+			reports: 'reports',
+			source:  'src',
+			tmp:     'tmp',
+			wpcom:   'wpcom',
 			global: {
-				config: require( 'path' ).join( __dirname, 'grunt' )
+				config: require( 'path' ).join( __dirname, 'config' ),
+				grunt:  require( 'path' ).join( __dirname, 'config/grunt' )
 			}
 		},
 		taskMap: {
