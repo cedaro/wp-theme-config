e42d8f774f61bdd66c289de905d803c38b53b110
Brady Vercher <brady@blazersix.com>
2015-03-18 10:56:54 -0700

    Exclude some package meta from POT files.

--- config/grunt/makepot.js
+++ config/grunt/makepot.js
@@ -11,6 +11,26 @@ module.exports = {
 				'poedit': true,
 				'report-msgid-bugs-to': 'https://audiotheme.com/support/'
 			},
+			processPot: function( pot ) {
+				var translation,
+					excluded_meta = [
+						'Theme Name of the plugin/theme',
+						'Theme URI of the plugin/theme',
+						'Author of the plugin/theme',
+						'Author URI of the plugin/theme'
+					];
+
+				for ( translation in pot.translations[''] ) {
+					if ( 'undefined' !== typeof pot.translations[''][ translation ].comments.extracted ) {
+						if ( excluded_meta.indexOf( pot.translations[''][ translation ].comments.extracted ) >= 0 ) {
+							console.log( 'Excluded meta: ' + pot.translations[''][ translation ].comments.extracted );
+							delete pot.translations[''][ translation ];
+						}
+					}
+				}
+
+				return pot;
+			},
 			type: 'wp-theme'
 		}
 	}
