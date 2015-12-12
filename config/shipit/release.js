c3a2c70ae3fc6799c4d7ca8505a7aa2dd8e80a8b
Brady Vercher <brady@blazersix.com>
2015-12-12 11:58:14 -0800

    Update the release task to upload packages to Amazon S3.

--- config/shipit/release.js
+++ config/shipit/release.js
@@ -1,11 +1,42 @@
-var path = require( 'path' );
+var aws = require('aws-sdk'),
+	fs = require('fs'),
+	path = require( 'path' );
 
 module.exports = function( shipit ) {
-	var project = require( path.join( process.cwd(), 'package.json' ) ),
+	var credentials, s3,
+		project = require( path.join( process.cwd(), 'package.json' ) ),
 		archiveFile = project.name + '-' + project.version + '.zip',
-		releasePath = shipit.config.releasesPath + project.name + '/' + archiveFile;
+		uploadPath = project.name + '/' + archiveFile;
 
-	shipit.log( 'Copying copy "dist/%s" to "%s"', archiveFile, releasePath );
+	if ( 'aws' !== shipit.environment || 's3' !== shipit.config.servers ) {
+		throw new Error( 'The release task only works with Amazon S3.' );
+		return;
+	}
 
-	return shipit.local( 'scp dist/' + archiveFile + ' ' + shipit.config.servers + ':' + releasePath );
+	credentials = new aws.SharedIniFileCredentials({
+		profile: shipit.config.profile || 'shipit'
+	});
+
+	s3 = new aws.S3({
+		credentials: credentials,
+		region: shipit.config.region || 'us-east-1'
+	});
+
+	shipit.log( 'Uploading "dist/%s" to "%s"', archiveFile, uploadPath );
+
+	return new Promise(function( resolve, reject ) {
+		s3.putObject({
+			ACL: 'public-read',
+			Bucket: shipit.config.bucket || 'audiotheme-packages',
+			Key: uploadPath,
+			Body: fs.readFileSync( 'dist/' + archiveFile ),
+			ContentType: 'application/zip',
+		}, function( error, response ) {
+			if ( error ) {
+				reject( error );
+			} else {
+				resolve( uploadPath );
+			}
+		});
+	});
 };
